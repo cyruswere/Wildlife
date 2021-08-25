8cd442309074468ac6cb0ca238dfc23bc007acab
cyruswere <cyrus.were@student.moringaschool.com>
2021-08-25 21:39:58 +0300

    added files

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 
 import models.*;
 import spark.ModelAndView;
-import spark.template.handlebars.HandlebarsTemplateEngine;
+//import spark.template.handlebars.HandlebarsTemplateEngine;
 
 import static java.lang.Integer.parseInt;
 
@@ -26,18 +26,18 @@ public class App {
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<>();
             return new ModelAndView(model, "index.hbs");
-        }, new HandlebarsTemplateEngine());
+        });
 
         get("/animals", (request, response) -> {
             Map<String, Object> model = new HashMap<>();
             model.put("animals", Animal.Animal_type);
             return new ModelAndView(model, "animal.hbs");
-        }, new HandlebarsTemplateEngine());
+        });
 
         get("/animals/new", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
             return new ModelAndView(model, "animal-form.hbs");
-        }, new HandlebarsTemplateEngine());
+        });
 
         post("/animal/new", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
@@ -51,12 +51,12 @@ public class App {
             newAnimal.save();
             model.put("animals", Animal.Animal_type);
             return new ModelAndView(model, "success.hbs");
-        }, new HandlebarsTemplateEngine());
+        });
 
         get("/endanger/new", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
             return new ModelAndView(model, "endan-form.hbs");
-        }, new HandlebarsTemplateEngine());
+        });
 
         post("/endangered/new", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
@@ -70,13 +70,13 @@ public class App {
             newAnimal.save();
             model.put("animals", Animal.Animal_type);
             return new ModelAndView(model, "success2.hbs");
-        }, new HandlebarsTemplateEngine());
+        });
 
         get("/sightings", (request, response) -> {
             Map<String, Object> model = new HashMap<>();
             model.put("sighting", Sighting.all());
             return new ModelAndView(model, "sight.hbs");
-        }, new HandlebarsTemplateEngine());
+        });
 
         get("/sightings/new", (request, response) -> {
             Map<String, Object> model = new HashMap<>();
@@ -85,7 +85,7 @@ public class App {
             model.put("endangered", Endangered.all());
             model.put("rangers", Ranger.all());
             return new ModelAndView(model, "sight-form.hbs");
-        }, new HandlebarsTemplateEngine());
+        });
 
         post("/sight/new" , (req,res) ->{
             Map<String, Object> model = new HashMap<>();
@@ -97,19 +97,19 @@ public class App {
             newSighting.save();
 //            model.put("sightings", Sighting.all());
             return new ModelAndView(model,"success-sight.hbs");
-        }, new HandlebarsTemplateEngine());
+        });
 
         get("/locations", (request, response) -> {
             Map<String, Object> model = new HashMap<>();
             model.put("location", Location.all());
             return new ModelAndView(model, "location.hbs");
-        }, new HandlebarsTemplateEngine());
+        });
 
         get("/locations/new", (req,res) ->{
             Map<String, Object> model = new HashMap<>();
             model.put("location", Location.all());
             return new ModelAndView(model,"location-form.hbs");
-        }, new HandlebarsTemplateEngine());
+        });
 
         post("/loc/new" , (req,res) ->{
             Map<String, Object> model = new HashMap<>();
@@ -121,6 +121,6 @@ public class App {
             newLocation.save();
 //            model.put("sightings", Sighting.all());
             return new ModelAndView(model,"loc-success.hbs");
-        }, new HandlebarsTemplateEngine());
+        });
     }
 }
