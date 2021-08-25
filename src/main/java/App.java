b92e10c5d9382e6426c3d720083b18cdf297acdf
cyruswere <cyrus.were@student.moringaschool.com>
2021-08-26 00:39:18 +0300

    added files

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,4 @@
-import models.Animal;
-import models.Endangered;
-import models.Location;
-import models.Ranger;
-import models.Sighting;
+
 import spark.ModelAndView;
 import spark.Spark;
 
@@ -34,7 +30,6 @@ public class App {
 
         Spark.get("/animals", (request, response) -> {
             Map<String, Object> model = new HashMap<>();
-            model.put("animals", Animal.Animal_type);
             return new ModelAndView(model, "animal.hbs");
         });
 
@@ -51,9 +46,6 @@ public class App {
             String age = req.queryParams("age");
             String health = req.queryParams("health");
             String location = req.queryParams("location");
-            Animal newAnimal = new Animal(id,name, type, age, health, location);
-            newAnimal.save();
-            model.put("animals", Animal.Animal_type);
             return new ModelAndView(model, "success.hbs");
         });
 
@@ -70,24 +62,17 @@ public class App {
             String age = req.queryParams("age");
             String health = req.queryParams("health");
             String location = req.queryParams("location");
-            Animal newAnimal = new Animal(id, name, type, age, health, location);
-            newAnimal.save();
-            model.put("animals", Animal.Animal_type);
             return new ModelAndView(model, "success2.hbs");
         });
 
         Spark.get("/sightings", (request, response) -> {
             Map<String, Object> model = new HashMap<>();
-            model.put("sighting", Sighting.all());
             return new ModelAndView(model, "sight.hbs");
         });
 
         Spark.get("/sightings/new", (request, response) -> {
             Map<String, Object> model = new HashMap<>();
 //            model.put("animals", Animal.all());
-            model.put("location", Location.all());
-            model.put("endangered", Endangered.all());
-            model.put("rangers", Ranger.all());
             return new ModelAndView(model, "sight-form.hbs");
         });
 
@@ -97,21 +82,17 @@ public class App {
             String location = req.queryParams("location");
             String rangername = req.queryParams("rangername");
             String animal_type = req.queryParams("animal_type");
-            Sighting newSighting =new Sighting (id,location,rangername,animal_type);
-            newSighting.save();
 //            model.put("sightings", Sighting.all());
             return new ModelAndView(model,"success-sight.hbs");
         });
 
         Spark.get("/locations", (request, response) -> {
             Map<String, Object> model = new HashMap<>();
-            model.put("location", Location.all());
             return new ModelAndView(model, "location.hbs");
         });
 
         Spark.get("/locations/new", (req, res) ->{
             Map<String, Object> model = new HashMap<>();
-            model.put("location", Location.all());
             return new ModelAndView(model,"location-form.hbs");
         });
 
@@ -121,8 +102,6 @@ public class App {
             String sightings_location = req.queryParams("sightings_location");
 //            String rangername = req.queryParams("rangername");
 //            String animal_type = req.queryParams("animal_type");
-            Location newLocation =new Location (sightings_location);
-            newLocation.save();
 //            model.put("sightings", Sighting.all());
             return new ModelAndView(model,"loc-success.hbs");
         });
